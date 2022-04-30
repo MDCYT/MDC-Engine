4640b530875840645eebc9e8ffa1febbbb488b68
Niz <76523353+nizako@users.noreply.github.com>
2022-04-30 11:50:11 -0500

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -49,7 +49,7 @@ class NativePaths
 	inline public static function image(from:String)
 	{
 		if (Assets.exists('assets/images/' + from))
-			return BitmapData.fromFile('assets/images/' + from);
+			return BitmapData.fromFile('assets/images/' + from + '.png');
 		else
 			return null;
 	}
@@ -64,7 +64,7 @@ class NativePaths
 		else if (Assets.exists("assets/" + from + ".xml"))
 			elxokas = ".xml";
 		if (Assets.exists('assets/' + from + elxokas))
-			return NativeFile.file_contents_string('assets/' + from);
+			return NativeFile.file_contents_string('assets/' + from + elxokas);
 		else
 			return "null";
 	}
