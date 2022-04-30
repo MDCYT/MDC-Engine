b7486da12b4f8fc5bfbfb56c9eee72719e6e6fc2
Manux123 <manuldestevez@gmail.com>
2022-04-30 12:37:09 -0500

    BV

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -146,6 +146,8 @@ class DialogueBox extends FlxSpriteGroup
 			portraitLeft.screenCenter(X);
 
 			handSelect = new FlxSprite(FlxG.width * 0.9, FlxG.height * 0.9).loadGraphic(Paths.image('weeb/pixelUI/hand_textbox'));
+			handSelect.setGraphicSize(Std.int(handSelect.width * 3));
+			handSelect.updateHitbox();
 			add(handSelect);
 
 			if (!talkingRight)
